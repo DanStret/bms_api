2e3c40f228b1c954ad8754a5a5158870e81bdc44
DanStret <ander4c9pi@gmail.com>
2025-02-07 11:49:21 -0500

    actualizacion final

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,50 @@
-from flask import Flask, render_template
-from flask_cors import CORS
+from flask import Flask, jsonify ,render_template
+from flask_cors import CORS  # Importa CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from config import config
-from datetime import datetime
-from collections import defaultdict
-from app.extensions import db, ma, migrate
+import pymysql
+
+# Configura pymysql para MySQL
+pymysql.install_as_MySQLdb()
+
+# Inicializa las extensiones
+db = SQLAlchemy()
+ma = Marshmallow()
+migrate = Migrate()
 
 def create_app(config_name='default'):
+    # Crea la aplicación Flask
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    CORS(app)
-    
+
+    # Configuración de CORS de manera global
+    CORS(app)  # Habilita CORS para todas las rutas y orígenes
+
+    # Imprime la configuración actual (para depuración)
+    print(f"Current config: {app.config['SQLALCHEMY_DATABASE_URI']}")
+
+    # Inicializa las extensiones con la aplicación
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
 
+    # Endpoint de prueba
+    @app.route('/api/test', methods=['GET'])
+    def test():
+        return jsonify({"message": "Hello from Render!"})
+
     with app.app_context():
+        # Importa los blueprints
         from .routes.buildings import buildings_bp
         from .routes.systems import systems_bp
         from .routes.data import data_bp
         from .routes.commands import commands_bp
         from .routes.signals import signals_bp
-        from .routes.modes import modes_bp
+        from.routes.modes import modes_bp
 
+        # Registra los blueprints
         app.register_blueprint(buildings_bp)
         app.register_blueprint(systems_bp)
         app.register_blueprint(data_bp)
